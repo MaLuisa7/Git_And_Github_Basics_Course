5d65e0a1283ab54d8e3733d1e30957666e1e74f3
MaLuisa7 <mluisa.args@gmail.com>
2023-04-21 12:37:39 -0600

    first python file  changed

--- firstpython.py
+++ firstpython.py
@@ -1,3 +1,5 @@
 #Display the output
 
 print("New python file")
+print("This change was done by Maria Luisa as part of the course git and github basics")
+print("In the part of using git from your own desktop")
